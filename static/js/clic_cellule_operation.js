9082b6ca46edda158a454591febc6bffbcdaad91
Mottais <mottais.afy@outlook.fr>
2024-06-12 17:39:12 +0200

    code de l'évenement 'click' des cellules déplacé dans une fonction 'miseAJourBoiteDeDialogue' pour pouvoir être appelée par d'autres scripts.

--- static/js/clic_cellule_operation.js
+++ static/js/clic_cellule_operation.js
@@ -1,7 +1,7 @@
 // Décmaration des variables globales
 
 // Valeur de progression pour le bouton "EN COURS"
-ValeurDeProgression = 10;
+let ValeurDeProgression = 10;
 
 // Cellules du tableau avec un attribut 'data-statut' différent de 'None' c'est-à-dire les cellules qui ont une opération
 const cells = document.querySelectorAll('td[data-statut]:not([data-statut="None"])');
@@ -13,49 +13,54 @@ const etapeSelect = document.getElementById('Etape_select');
 const textSelect = document.getElementById('Texte_select');
 const statutSelect = document.getElementById('Statut_select');
 
+// Fonction de gestionnaire d'événements
+function miseAJourBoiteDeDialogue(cell) {
+    // Vérification si l'ID de la cellule est différent de 'None'
+    if (cell.id !== 'None') {
+        // Mise à jour des valeurs de la boîte de dialogue avec les données de la cellule
+        opSelect.textContent = cell.getAttribute('id');
+        paquetSelect.textContent = cell.getAttribute('data-paquet');
+        etapeSelect.textContent = cell.getAttribute('data-etape');
+        textSelect.textContent = cell.textContent;
+        statutSelect.textContent = cell.getAttribute('data-statut') + "%";
+
+        // Supprime l'attribut 'selected' de l'élément actuellement sélectionné (s'il existe)
+        if (document.querySelector('[selected="true"]')) {document.querySelector('[selected="true"]').removeAttribute('selected');}
+
+        // création d'un attribut 'selected' pour la cellule cliquée avec la valeur 'true'
+        cell.setAttribute('selected', 'true');
+
+        let statut = cell.getAttribute('data-statut');
+
+        if (statut == 0) {
+            document.getElementById('bouton_A_FAIRE').style.visibility = 'hidden';
+            document.getElementById('bouton_EN_COURS').style.visibility = 'visible';
+            document.getElementById('bouton_FAIT').style.visibility = 'visible';
+        }
+
+        if (statut > 0 && statut < (100 - ValeurDeProgression)) {
+            document.getElementById('bouton_A_FAIRE').style.visibility = 'visible';
+            document.getElementById('bouton_EN_COURS').style.visibility = 'visible';
+            document.getElementById('bouton_FAIT').style.visibility = 'visible';
+        }
+
+        if (statut >= 90 && statut < 100) {
+            document.getElementById('bouton_A_FAIRE').style.visibility = 'visible';
+            document.getElementById('bouton_EN_COURS').style.visibility = 'hidden';
+            document.getElementById('bouton_FAIT').style.visibility = 'visible';
+        }
+
+        if (statut == 100) {
+            document.getElementById('bouton_A_FAIRE').style.visibility = 'visible';
+            document.getElementById('bouton_EN_COURS').style.visibility = 'hidden';
+            document.getElementById('bouton_FAIT').style.visibility = 'hidden';
+        }
+    }
+}
+
 // Ajout d'un gestionnaire d'événement pour chaque cellule
 cells.forEach(function(cell) {
     cell.addEventListener('click', function() {
-        // Vérification si l'ID de la cellule est différent de 'None'
-        if (this.id !== 'None') {
-            // Mise à jour des valeurs de la boîte de dialogue avec les données de la cellule
-            opSelect.textContent = this.getAttribute('id');
-            paquetSelect.textContent = this.getAttribute('data-paquet');
-            etapeSelect.textContent = this.getAttribute('data-etape');
-            textSelect.textContent = this.textContent;
-            statutSelect.textContent = this.getAttribute('data-statut') + "%";
-
-            // Supprime l'attribut 'selected' de l'élément actuellement sélectionné (s'il existe)
-            if (document.querySelector('[selected="true"]')) {document.querySelector('[selected="true"]').removeAttribute('selected');}
-
-            // création d'un attribut 'selected' pour la cellule cliquée avec la valeur 'true'
-            this.setAttribute('selected', 'true');
-
-            let statut = this.getAttribute('data-statut');
-
-            if (statut == 0) {
-                document.getElementById('bouton_A_FAIRE').style.visibility = 'hidden';
-                document.getElementById('bouton_EN_COURS').style.visibility = 'visible';
-                document.getElementById('bouton_FAIT').style.visibility = 'visible';
-            }
-
-            if (statut > 0 && statut < (100 - ValeurDeProgression)) {
-                document.getElementById('bouton_A_FAIRE').style.visibility = 'visible';
-                document.getElementById('bouton_EN_COURS').style.visibility = 'visible';
-                document.getElementById('bouton_FAIT').style.visibility = 'visible';
-            }
-
-            if (statut >= 90 && statut < 100) {
-                document.getElementById('bouton_A_FAIRE').style.visibility = 'visible';
-                document.getElementById('bouton_EN_COURS').style.visibility = 'hidden';
-                document.getElementById('bouton_FAIT').style.visibility = 'visible';
-            }
-
-            if (statut == 100) {
-                document.getElementById('bouton_A_FAIRE').style.visibility = 'visible';
-                document.getElementById('bouton_EN_COURS').style.visibility = 'hidden';
-                document.getElementById('bouton_FAIT').style.visibility = 'hidden';
-            }
-        }
+        miseAJourBoiteDeDialogue(cell);
     });
 });
