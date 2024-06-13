cc6da8866989ebfd2a084efb440e87c82415a374
Mottais <mottais.afy@outlook.fr>
2024-06-13 18:06:11 +0200

    Modification du type de déclaration des variables

--- static/js/TempsReel_Operation_change.js
+++ static/js/TempsReel_Operation_change.js
@@ -1,4 +1,4 @@
-    var socket = io();
+    let socket = io();
 
     // Test la connexion au serveur WebSocket
     // socket.on('connect', function() {
@@ -7,15 +7,15 @@
 
     socket.on('update_status', function(data) {
         // alert(`événement 'update_status' reçu avec id: ${data.id} et statut: ${data.nouveau_statut}`);
-        var operationId = data.id;
-        var nouveauStatut = data.nouveau_statut;
+        let operationId = data.id;
+        let nouveauStatut = data.nouveau_statut;
 
-        var cell = document.getElementById(operationId);
+        let cell = document.getElementById(operationId);
         if (cell) {
           // alert(`Cellule trouvée pour id: ${operationId} et mis àjour dans cette page`);
           cell.dataset.statut = nouveauStatut;
           // cell.innerText = nouveauStatut;
-          var elementSelected = document.querySelector('[selected="true"]');
+          let elementSelected = document.querySelector('[selected="true"]');
           if (elementSelected.id == operationId) { miseAJourBoiteDeDialogue(elementSelected)}
         } // else {
         //   alert(`Cellule non trouvée pour id: ${operationId}`);
