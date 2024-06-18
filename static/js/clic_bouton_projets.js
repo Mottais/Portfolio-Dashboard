a422544a88e03518f720fb6b6817ba7a3036c5f6
Mottais <mottais.afy@outlook.fr>
2024-06-18 16:15:17 +0200

    Affichage de la liste de choix via clic bouton 'Projet' et non pas via le survol car pas possible sur smartphone ou tablette

--- static/js/clic_bouton_projets.js
+++ static/js/clic_bouton_projets.js
@@ -1,4 +1,19 @@
 // Rafraîchit la page lorsqu'on clique sur le bouton "Projets" mettant à jour la liste des projets
+let dropdown = document.querySelector('#dropdown ul');
+
 document.getElementById('dropdown').addEventListener('click', function() {
-  location.reload();
+  // location.reload();
+  if (dropdown.style.display === 'block') {
+    dropdown.style.display = 'none';
+  } else {
+    dropdown.style.display = 'block';
+  }
+});
+
+let listItems = document.querySelectorAll('#dropdown ul li');
+listItems.forEach(function(li) {
+  li.addEventListener('click', function() {
+    let dropdown = document.querySelector('#dropdown ul');
+    dropdown.style.display = 'none';
+  });
 });
