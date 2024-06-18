582e7b619c45b8596903bb065e939ed09a6e6197
Mottais <mottais.afy@outlook.fr>
2024-06-18 11:43:51 +0200

    Rafraîchir la page lorsqu'on clique sur le bouton "Projets" mettant à jour la liste des projets

---
+++ static/js/clic_bouton_projets.js
@@ -0,0 +1,4 @@
+// Rafraîchit la page lorsqu'on clique sur le bouton "Projets" mettant à jour la liste des projets
+document.getElementById('dropdown').addEventListener('click', function() {
+  location.reload();
+});
