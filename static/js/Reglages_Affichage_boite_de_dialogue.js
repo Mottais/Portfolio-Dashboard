a32974e9ff6b090a9b787f8cf75a07f66a7b13f9
Mottais <mottais.afy@outlook.fr>
2024-06-24 12:23:02 +0200

    Sauvegarder et Restaurer l'état des boutons d'affichage au chargement de la page en fonction des valeurs stockées dans le localStorage

--- static/js/Reglages_Affichage_boite_de_dialogue.js
+++ static/js/Reglages_Affichage_boite_de_dialogue.js
@@ -10,9 +10,11 @@ function AffichageEtape(button) {
     if (button.classList.contains('active')) {
         button.classList.remove('active');
         document.getElementById('Etape_select').style.display = 'none';
+        localStorage.setItem('etatBoutonEtape', 'inactif');
     } else {
         button.classList.add('active');
         document.getElementById('Etape_select').style.display = 'flex';
+        localStorage.setItem('etatBoutonEtape', 'actif');
     }
 }
 
@@ -20,9 +22,11 @@ function AffichagePaquet(button) {
     if (button.classList.contains('active')) {
         button.classList.remove('active');
         document.getElementById('Paquet_select').style.display = 'none';
+        localStorage.setItem('etatBoutonPaquet', 'inactif');
     } else {
         button.classList.add('active');
         document.getElementById('Paquet_select').style.display = 'flex';
+        localStorage.setItem('etatBoutonPaquet', 'actif');
     } 
 }
 
@@ -30,9 +34,11 @@ function AffichageProfil(button) {
     if (button.classList.contains('active')) {
         button.classList.remove('active');
         document.getElementById('Profil_select').style.display = 'none';
+        localStorage.setItem('etatBoutonProfil', 'inactif');
     } else {
         button.classList.add('active');
         document.getElementById('Profil_select').style.display = 'flex';
+        localStorage.setItem('etatBoutonProfil', 'actif');
     } 
 }
 
@@ -40,9 +46,11 @@ function AffichageCompte(button) {
     if (button.classList.contains('active')) {
         button.classList.remove('active');
         document.getElementById('Compte_select').style.display = 'none';
+        localStorage.setItem('etatBoutonCompte', 'inactif');
     } else {
         button.classList.add('active');
         document.getElementById('Compte_select').style.display = 'flex';
+        localStorage.setItem('etatBoutonCompte', 'actif');
     } 
 }
 
@@ -50,17 +58,19 @@ function AffichageStatut(button) {
     if (button.classList.contains('active')) {
         button.classList.remove('active');
         document.getElementById('Statut_select').style.display = 'none';
+        localStorage.setItem('etatBoutonStatut', 'inactif');
     } else {
         button.classList.add('active');
         document.getElementById('Statut_select').style.display = 'flex';
+        localStorage.setItem('etatBoutonStatut', 'actif');
     } 
 }
 
 
-const btnEtape = document.getElementById('bouton_AffichageEtape');
-
-
-// Affichage par féfaut dans la boite de dialogue
-document.getElementById('bouton_AffichageEtape').click();
-document.getElementById('bouton_AffichagePaquet').click();
+// Restaurer l'état des boutons d'affichage au chargement de la page en fonction des valeurs stockées dans le localStorage   
+if (localStorage.getItem('etatBoutonPaquet') === 'actif') { document.getElementById('bouton_AffichagePaquet').click(); }
+if (localStorage.getItem('etatBoutonEtape') === 'actif') { document.getElementById('bouton_AffichageEtape').click(); }
+if (localStorage.getItem('etatBoutonProfil') === 'actif') { document.getElementById('bouton_AffichageProfil').click(); }
+if (localStorage.getItem('etatBoutonCompte') === 'actif') { document.getElementById('bouton_AffichageCompte').click(); }
+if (localStorage.getItem('etatBoutonStatut') === 'actif') { document.getElementById('bouton_AffichageStatut').click(); }
 
