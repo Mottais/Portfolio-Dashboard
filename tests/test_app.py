1a1435fbd0abecc5d49da7b312038951b1522f4b
Mottais <mottais.afy@outlook.fr>
2024-07-08 19:09:09 +0200

    modif commentaires pour respecter les directives de style de code de PEP 8 (pycodestyle)

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,64 +11,78 @@ from app import app, AccesBDD
 class BasicTests(unittest.TestCase):
 
     def setUp(self):
+        """
+        Configurer l'application Flask pour les tests.
+        """
         self.app = app
         self.client = self.app.test_client()
         self.app.config['TESTING'] = True
 
     def test_login_get(self):
-        # Test de la route /login en mode GET
+        """
+        Test de la route /login en mode GET.
+        """
         response = self.client.get('/login')
         self.assertEqual(response.status_code, 200)
         self.assertIn(b'<title>Login</title>', response.data)
 
-    # Simulation méthode get_compte_by_nom_pw retournant compte fictif
     @patch.object(AccesBDD, 'get_compte_by_nom_pw',
                   return_value={'id': 1, 'nom': 'test_user', 'pw': 'test_pw'})
     def test_login_post(self, mock_get_compte):
-        # Test de la route /login en mode POST avec un compte fictif
+        """
+        Test de la route /login en mode POST avec un compte fictif.
+        """
         response = self.client.post('/login',
                                     data=dict(nom='test_user', pw='test_pw'))
-        # Redirection 302 attendue après une connexion réussie
         self.assertEqual(response.status_code, 302)
         self.assertIn('/Tableau_de_bord_JARNOT/Projets/',
                       response.headers['Location'])
 
     def test_login_post_admin(self):
-        # Test de la route /login en mode POST avec le compte administrateur
+        """
+        Test de la route /login en mode POST avec le compte administrateur.
+        """
         response = self.client.post(
             '/login', data=dict(nom='admin', mot_de_passe='pwadmin'))
-        # Redirection 302 attendue après une connexion réussie
         self.assertEqual(response.status_code, 302)
         self.assertIn('/Tableau_de_bord_JARNOT/Projets/',
                       response.headers['Location'])
 
     @patch.object(AccesBDD, 'get_compte_by_nom_pw', return_value=None)
     def test_login_post_invalid(self, mock_get_compte):
-        # Test de la route /login en mode POST avec un compte inexistant
+        """
+        Test de la route /login en mode POST avec un compte inexistant.
+        """
         response = self.client.post(
             '/login', data=dict(nom='wrong_user', mot_de_passe='wrong_pw'))
         self.assertEqual(response.status_code, 200)
-        # Encodage UTF-8 car le texte contient des caractères spéciaux
-        self.assertIn('Connexion échouée. Vérifiez vos identifiants.'.encode(
-            'utf-8'), response.data)
+        expected_message = 'Connexion échouée. Vérifiez vos identifiants.'.encode('utf-8')
+        self.assertIn(expected_message, response.data)
 
     def test_login_post_invalid_pw(self):
-        # Test de la route /login en mode POST avec un mot de passe incorrect
+        """
+        Test de la route /login en mode POST avec un mot de passe incorrect.
+        """
         response = self.client.post(
             '/login', data=dict(nom='wrong_user', pw='wrong_pw'))
         self.assertEqual(response.status_code, 200)
-        self.assertIn('Connexion échouée. Vérifiez vos identifiants.'.encode(
-            'utf-8'), response.data)
+        expected_message = 'Connexion échouée. Vérifiez vos identifiants.'.encode('utf-8')
+        self.assertIn(expected_message, response.data)
 
     def test_logout(self):
-        # Test de la route /logout
-        self.client.post('/login', data=dict(nom='test_user',
-                         mot_de_passe='test_pw'), follow_redirects=True)
+        """
+        Test de la route /logout.
+        """
+        self.client.post('/login',
+                         data=dict(nom='test_user', mot_de_passe='test_pw'),
+                         follow_redirects=True)
         response = self.client.get('/logout', follow_redirects=True)
         self.assertIn(b'Login', response.data)
 
     def test_liste_projetsx(self):
-        # Test de la route /Tableau_de_bord_JARNOT/Liste_Projets/
+        """
+        Test de la route /Tableau_de_bord_JARNOT/Liste_Projets/.
+        """
         response = self.client.get('/Tableau_de_bord_JARNOT/Liste_Projets/')
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.is_json)
@@ -76,7 +90,9 @@ class BasicTests(unittest.TestCase):
     @patch.object(AccesBDD, 'get_compte_by_nom_pw',
                   return_value={'id': 1, 'nom': 'test_user', 'pw': 'test_pw'})
     def test_detail_projet(self, mock_get_compte):
-        # Test de la route /Tableau_de_bord_JARNOT/Projets/1
+        """
+        Test de la route /Tableau_de_bord_JARNOT/Projets/1.
+        """
         self.client.post('/login', data=dict(nom='test_user', pw='test_pw'))
         response = self.client.get('/Tableau_de_bord_JARNOT/Projets/1')
         self.assertEqual(response.status_code, 200)
@@ -86,7 +102,9 @@ class BasicTests(unittest.TestCase):
     @patch.object(AccesBDD, 'get_compte_by_nom_pw',
                   return_value={'id': 1, 'nom': 'test_user', 'pw': 'test_pw'})
     def test_detail_projet_sans_id(self, mock_get_compte):
-        # Test de la route /Tableau_de_bord_JARNOT/Projets/ sans id
+        """
+        Test de la route /Tableau_de_bord_JARNOT/Projets/ sans id.
+        """
         self.client.post('/login', data=dict(nom='test_user', pw='test_pw'))
         response = self.client.get('/Tableau_de_bord_JARNOT/Projets/')
         self.assertEqual(response.status_code, 200)
@@ -96,7 +114,9 @@ class BasicTests(unittest.TestCase):
     @patch.object(AccesBDD, 'get_compte_by_nom_pw',
                   return_value={'id': 1, 'nom': 'test_user', 'pw': 'test_pw'})
     def test_detail_projet_sans_errone(self, mock_get_compte):
-        # Test de la route /Tableau_de_bord_JARNOT/Projets/ id erroné
+        """
+        Test de la route /Tableau_de_bord_JARNOT/Projets/ id erroné.
+        """
         self.client.post('/login', data=dict(nom='test_user', pw='test_pw'))
         response = self.client.get('/Tableau_de_bord_JARNOT/Projets/id_errone')
         self.assertEqual(response.status_code, 404)
@@ -104,8 +124,10 @@ class BasicTests(unittest.TestCase):
 
     @patch.object(AccesBDD, 'get_compte_by_nom_pw',
                   return_value={'id': 1, 'nom': 'test_user', 'pw': 'test_pw'})
-    def test_update_statut(self,mock_get_compte):
-        # Test de la route /Tableau_de_bord_JARNOT/update_statut/
+    def test_update_statut(self, mock_get_compte):
+        """
+        Test de la route /Tableau_de_bord_JARNOT/update_statut/.
+        """
         self.client.post('/login', data=dict(nom='test_user', pw='test_pw'))
         response = self.client.put('/Tableau_de_bord_JARNOT/update_statut/',
                                    json={'id': 1, 'nouveau_statut': 101})
@@ -114,31 +136,24 @@ class BasicTests(unittest.TestCase):
         self.assertTrue(response.is_json)
         self.assertTrue(response.json['success'])
         self.client.post('/login', data=dict(nom='test_user', pw='test_pw'))
-        # modification du statut de l'op pour pouvoir relancer le test sans echec
+        # modification statut de l'op pour pouvoir relancer le test sans echec
         response = self.client.put('/Tableau_de_bord_JARNOT/update_statut/',
                                    json={'id': 1, 'nouveau_statut': 100})
 
     @patch.object(AccesBDD, 'get_compte_by_nom_pw',
                   return_value={'id': 1, 'nom': 'test_user', 'pw': 'test_pw'})
     def test_update_statut_id_inconnu(self, mock_get_compte):
-        # Test de la route /Tableau_de_bord_JARNOT/update_statut/ id inconnu
+        """
+        Test de la route /Tableau_de_bord_JARNOT/update_statut/ id inconnu.
+        """
         self.client.post('/login', data=dict(nom='test_user', pw='test_pw'))
         response = self.client.put('/Tableau_de_bord_JARNOT/update_statut/',
-                                   json={'id': 9999999999999, 'nouveau_statut': 100})
+                                   json={'id': 9999999, 'nouveau_statut': 100})
         self.assertEqual(response.status_code, 200)
         print(response.json)
         self.assertFalse(response.json['success'])
 
-    # @patch.object(AccesBDD, 'get_compte_by_nom_pw',
-    #               return_value={'id': 1, 'nom': 'test_user', 'pw': 'test_pw'})
-    # def test_update_statut_id_errone(self, mock_get_compte):
-    #     self.client.post('/login', data=dict(nom='test_user', pw='test_pw'))
-    #     response = self.client.put('/Tableau_de_bord_JARNOT/update_statut/',
-    #                                json={'id': 1, 'nouveau_statut': 'texte'})
-    #     self.assertEqual(response.status_code, 200)
-    #     print(response.json)
-    #     self.assertFalse(response.json['success'])
-
 
 if __name__ == "__main__":
+    # Exécute les tests unitaires
     unittest.main()
--- tests/test_models.py
+++ tests/test_models.py
@@ -2,21 +2,26 @@ import unittest
 import MySQLdb
 from datetime import datetime
 from models import AccesBDD
-import os
+
 
 class TestAccesBDD(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
+        """Initialisation des tests de classe."""
         cls.base_de_donnees = AccesBDD('TABLEAU_DE_BORD_JARNOT_TEST')
-        cls._execute_sql_script('./MySQL/TABLEAU_DE_BORD_JARNOT_TEST_1_Créat_BD_et_ajout_données.sql')
+        cls._execute_sql_script(
+            './MySQL/TABLEAU_DE_BORD_JARNOT_TEST_1_Créat_BD_et_ajout_données.sql'
+        )
 
     @classmethod
     def tearDownClass(cls):
+        """Nettoyage après les tests de classe."""
         cls.base_de_donnees.connexion_close()
 
     @classmethod
     def _execute_sql_script(cls, script_path):
+        """Exécute un script SQL pour initialiser la base de données."""
         # Connexion à la base de données
         conn = MySQLdb.connect(
             host="localhost",
@@ -26,97 +31,112 @@ class TestAccesBDD(unittest.TestCase):
             db='TABLEAU_DE_BORD_JARNOT_TEST'
         )
         cursor = conn.cursor()
-        
+
         # Lire le fichier SQL
         with open(script_path, 'r') as file:
             sql_script = file.read()
-        
+
         # Exécuter le script SQL
         for statement in sql_script.split(';'):
             if statement.strip():
                 cursor.execute(statement)
-        
+
         # Valider les modifications et fermer la connexion
         conn.commit()
         cursor.close()
         conn.close()
 
     def test_get_compte_by_id_existant(self):
-        # test de la fonction get_compte_by_id avec id existant
+        """Test de get_compte_by_id avec un ID existant."""
         compte = self.base_de_donnees.get_compte_by_id(1)
         self.assertEqual(compte, {'id': 1, 'nom': 'admin', 'pw': 'pwadmin'})
 
     def test_get_compte_by_id_inexistant(self):
-        # test de la fonction get_compte_by_id avec id inexistant
+        """Test de get_compte_by_id avec un ID inexistant."""
         compte = self.base_de_donnees.get_compte_by_id(5)
         self.assertNotEqual(compte, {None})
 
     def test_get_compte_by_id_non_valide(self):
-        # test de la fonction get_compte_by_id avec id non valide
+        """Test de get_compte_by_id avec un ID non valide."""
         compte = self.base_de_donnees.get_compte_by_id('non_valid_id')
         self.assertEqual(compte, {None})
 
     def test_get_compte_by_nom_pw_existant(self):
-        # test de la fonction get_compte_by_nom_pw avec nom et pw existant
+        """Test de get_compte_by_nom_pw avec nom et mot de passe existants."""
         compte = self.base_de_donnees.get_compte_by_nom_pw('admin', 'pwadmin')
         self.assertEqual(compte, {'id': 1, 'nom': 'admin', 'pw': 'pwadmin'})
 
     def test_get_profils_user_existant(self):
-        # test de la fonction get_profils_user avec nom existant
+        """Test de get_profils_user avec un nom existant."""
         profils = self.base_de_donnees.get_profils_user('admin')
-        self.assertEqual(profils, ['Atelier Bois', 'Atelier Zinguerie', 'Magazin', 'Chauffeur', 'Chantier', 'Conducteur travaux'])
+        self.assertEqual(
+            profils,
+            ['Atelier Bois', 'Atelier Zinguerie', 'Magazin',
+             'Chauffeur', 'Chantier', 'Conducteur travaux']
+        )
 
     def test_get_profils_user_non_existant(self):
-        # test de la fonction get_profils_user avec nom inexistant
+        """Test de get_profils_user avec un nom inexistant."""
         profils = self.base_de_donnees.get_profils_user('toto')
         self.assertEqual(profils, ['Atelier Zinguerie'])
 
     def test_get_all_projets_avant_01012024(self):
-        # test de la fonction get_all_projets date avant 01/01/2024
+        """Test de get_all_projets avec une date avant le 01/01/2024."""
         nb_jours = (datetime.today() - datetime(2024, 1, 1)).days
         liste_prj_en_cours = self.base_de_donnees.get_all_projets(nb_jours + 1)
         self.assertEqual(len(liste_prj_en_cours), 3)
 
     def test_get_all_projets_apres_01012024(self):
-        # test de la fonction get_all_projets date après 01/01/2024
+        """Test de get_all_projets avec une date après le 01/01/2024."""
         nb_jours = (datetime.today() - datetime(2024, 1, 1)).days
         liste_prj_en_cours = self.base_de_donnees.get_all_projets(nb_jours)
         self.assertEqual(len(liste_prj_en_cours), 2)
-        self.assertEqual(liste_prj_en_cours[0]['nb_ops_non_finies'], 3)
-        self.assertEqual(liste_prj_en_cours[0], {'id': 1, 'nom': 'Projet TEST A', 'version': None, 'nb_ops_non_finies': 3})
+        self.assertEqual(
+            liste_prj_en_cours[0]['nb_ops_non_finies'], 3
+        )
+        self.assertEqual(
+            liste_prj_en_cours[0],
+            {'id': 1, 'nom': 'Projet TEST A',
+             'version': None, 'nb_ops_non_finies': 3}
+        )
 
     def test_get_all_projets_parametre_non_valide(self):
-        # test de la fonction get_all_projets avec un paramètre non valide
-        liste_prj_en_cours = self.base_de_donnees.get_all_projets('non_valid_nb_jours')
+        """Test de get_all_projets avec un paramètre non valide."""
+        liste_prj_en_cours = self.base_de_donnees.get_all_projets('non_valid')
         self.assertEqual(len(liste_prj_en_cours), 2)
 
     def test_get_info_projet_by_id_existant(self):
-        # test de la fonction get_info_projet_by_id avec id existant
+        """Test de get_info_projet_by_id avec un ID existant."""
         info_prj = self.base_de_donnees.get_info_projet_by_id(1)
-        self.assertEqual(info_prj, {'id': 1, 'nom': 'Projet TEST A', 'version': None, 'adresse': '123 Rue des Fleurs', 'cp': '35000', 'ville': 'RENNES'})
+        self.assertEqual(
+            info_prj,
+            {'id': 1, 'nom': 'Projet TEST A', 'version': None,
+             'adresse': '123 Rue des Fleurs', 'cp': '35000',
+             'ville': 'RENNES'}
+        )
 
     def test_get_info_projet_by_id_inexistant(self):
-        # test de la fonction get_info_projet_by_id avec id inexistant
+        """Test de get_info_projet_by_id avec un ID inexistant."""
         info_prj = self.base_de_donnees.get_info_projet_by_id(4)
         self.assertEqual(info_prj, {None})
 
     def test_get_info_projet_by_id_non_valide(self):
-        # test de la fonction get_info_projet_by_id avec id non valide
+        """Test de get_info_projet_by_id avec un ID non valide."""
         info_prj = self.base_de_donnees.get_info_projet_by_id('non_valid_id')
         self.assertEqual(info_prj, {None})
 
     def test_get_ops_by_id_projet_existant(self):
-        # test de la fonction get_ops_by_id_projet avec id existant
+        """Test de get_ops_by_id_projet avec un ID de projet existant."""
         liste_op_prj = self.base_de_donnees.get_ops_by_id_projet(3)
         self.assertEqual(len(liste_op_prj), 19)
 
     def test_get_ops_by_id_projet_non_valide(self):
-        # test de la fonction get_ops_by_id_projet avec id non valide
+        """Test de get_ops_by_id_projet avec un ID de projet non valide."""
         liste_op_prj = self.base_de_donnees.get_ops_by_id_projet('non_valid_id')
         self.assertEqual(len(liste_op_prj), 0)
 
     def test_update_statut_operation(self):
-        # test de la update_statut_operation
+        """Test de update_statut_operation."""
         liste_prj_en_cours = self.base_de_donnees.get_all_projets(0)
         self.assertEqual(len(liste_prj_en_cours), 2)
         self.base_de_donnees.update_statut_operation(7, 100, 'testeur')
@@ -127,6 +147,7 @@ class TestAccesBDD(unittest.TestCase):
         self.base_de_donnees.update_statut_operation(9, 99, 'testeur')
         liste_prj_en_cours = self.base_de_donnees.get_all_projets(0)
         self.assertEqual(len(liste_prj_en_cours), 2)
+
 
 if __name__ == '__main__':
     unittest.main()
