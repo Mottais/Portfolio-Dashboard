039eac535de9e812f91304841a40eda57cb3b908
Mottais <mottais.afy@outlook.fr>
2025-05-23 17:58:42 +0200

    Ajout de l'utilisateur 'USER' et des ces droits pour la base de données de test

--- MySQL/TABLEAU_DE_BORD_JARNOT_TEST_2_Création_Utilisateur_DEV.sql
+++ MySQL/TABLEAU_DE_BORD_JARNOT_TEST_2_Création_Utilisateur_DEV.sql
@@ -1,8 +1,12 @@
 -- A exécuter dans la console :
--- cat TABLEAU_DE_BORD_JARNOT_TEST_2_Création_Utilisateur_DEV.sql | sudo mysql -u root -pyannick
+-- cat *TErr
 
 
 
 CREATE USER IF NOT EXISTS 'DEV'@'localhost' IDENTIFIED BY 'pw@DEV';
 GRANT ALL PRIVILEGES ON TABLEAU_DE_BORD_JARNOT_TEST.* TO 'DEV'@'localhost';
+FLUSH PRIVILEGES;
+
+CREATE USER IF NOT EXISTS 'USER'@'localhost' IDENTIFIED BY 'pw@USER';
+GRANT ALL PRIVILEGES ON TABLEAU_DE_BORD_JARNOT_TEST.* TO 'USER'@'localhost';
 FLUSH PRIVILEGES;
